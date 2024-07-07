dbf5c893a29d005fa402de9447a012ce4cf647db
xsamskellington <samantha.valdez.toccalino@gmail.com>
2024-07-07 02:59:17 -0300

    starting soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,10 @@
-import type { Metadata } from 'next';
-import styles from './page.module.css';
-
-export const metadata: Metadata = {
-  title: 'OUTER',
-};
+import './styles.css';
 
 export default function Home() {
   return (
-    <html>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body>
-        <main className={styles.main}>
-          <h1>OUTER</h1>
-          <p>SOON</p>
-        </main>
-      </body>
-    </html>
+    <div className="container">
+      <h1 className="title">OUTER</h1>
+      <p>SOON</p>
+    </div>
   );
 }
