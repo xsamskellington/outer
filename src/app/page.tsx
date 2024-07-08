f5322f27ff6ebd1e70cb0b10c1e8c94e3eb620ff
xsamskellington <samantha.valdez.toccalino@gmail.com>
2024-07-08 17:32:52 -0300

    probando video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import './styles.css';
 export default function Home() {
   return (
     <div className="container">
-      <h1 className="title">OUTER</h1>
-      <p>SOON</p>
       <video
         width="600"
         autoPlay
