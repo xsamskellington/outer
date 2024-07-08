608f1e10cd6f8ba77ea4d41f7ee57553245958c3
xsamskellington <samantha.valdez.toccalino@gmail.com>
2024-07-08 17:30:52 -0300

    probando video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,17 @@ export default function Home() {
     <div className="container">
       <h1 className="title">OUTER</h1>
       <p>SOON</p>
+      <video
+        width="600"
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{ display: 'block' }}
+      >
+        <source src="/video/vhs_outer.MP4" type="video/mp4" />
+        Tu navegador no soporta el elemento de video.
+      </video>
     </div>
   );
 }
